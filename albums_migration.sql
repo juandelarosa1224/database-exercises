3f5802d847635000be003374e1b90e47d9651e67
J.R <juan.delarosa1224@gmail.com>
2018-11-30 11:29:13 -0600

    albums_migration.sql

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS codeup_test_db;
+CREATE DATABASE codeup_test_db;
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+  id INT UNASIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  artist VARCHAR (50),
+  name VARCHAR(100),
+  release_date INT,
+  sales FLOAT,
+  genre VARCHAR(50)
+);
